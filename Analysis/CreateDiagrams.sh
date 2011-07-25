a34c1a9724416fed03302492d0913ebfebb4b579
Lukas Lentner <kontakt@lukaslentner.de>
2011-07-25 13:23:00 +0200

    Integrated old Repository

--- Analysis/CreateDiagrams.sh
+++
@@ -1,3 +0,0 @@
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-cat $SCRIPTPATH/CreateDiagrams.gnuplot | gnuplot
--- Compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-c++ Source/QHS.cpp -g -lgsl -lgslcblas -lm -fstack-protector-all -o QHS
